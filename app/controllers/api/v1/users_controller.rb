185be7eb017f370fa914f500c946e08198c71148
oazzat <oazzat19@gmail.com>
2019-01-02 16:59:30 -0500

    almost done

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,2 +1,18 @@
 class Api::V1::UsersController < ApplicationController
+
+  def index
+      @users = User.all
+    render json: @users
+  end
+
+  def create
+
+    user = User.create(user_params)
+    render json: user
+  end
+
+end
+
+def user_params
+  params.permit(:name)
 end
